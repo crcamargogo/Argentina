1d1044b5194fbe0ad1e1291c7bc44102c666f94f
Cristian Camilo Camargo <camargocristiancamilo11@gmail.com>
2025-03-10 10:48:23 -0500

    SE AGREGAN COMANDANTES A LA WEB_2_1_3_4_7_4

--- script.js
+++ script.js
@@ -180,7 +180,7 @@ function createWheel(selectedParticipant) {
     const colors = {
 
         "COMANDANTE": "#3498db",
-        " SOPORTE AEROESPACIAL": "#3498db",
+        "SOPORTE AEROESPACIAL": "#3498db",
         "FACILITADOR": "#3498db",
         "ÁGIL Y SIMPLE": "#2ecc71", 
         "En equipo ganamos": "#e74c3c",
